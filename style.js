766476ad4135800c8df0800ba93d199904ee7fc4
Emma Paris <sudeepacharjeegp@gmail.com>
2023-05-14 01:24:31 +0530

    Added

--- style.js
+++ style.js
@@ -11,3 +11,20 @@ function sendEmail() {
     "mailto:sudeepacharjeegp@gmail.com?subject=Contact Sudeep Acharjee"
   );
 }
+var icon1 = document.getElementById("icon1");
+         icon1.onclick = function () {
+            document.body.classList.toggle("dark-theme")           
+        
+         if(document.body.classList.contains("dark-theme")){
+            icon1.src="dark theme icon/sun.png";
+         }
+         else{
+            icon1.src="dark theme icon/moon.png";
+         }
+          if(!body.classList.contains("dark")){
+            localStorage.setItem("mode" , "light-mode");
+        }else{
+            localStorage.setItem("mode" , "dark-mode");
+        }
+      
+        }
